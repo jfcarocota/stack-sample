94bb4f726aca2a25c193df4577fb86e0cde7be45
jfcarocota <jfcarocota@gmail.com>
2020-02-03 09:32:45 -0700

    stack publish
    first commit of the stack sample for the course of c++

---
+++ src/Node.hh
@@ -0,0 +1,19 @@
+class Node
+{
+private:
+    /* data */
+    int data;
+    Node *next;
+public:
+    node(/* args */);
+    ~node();
+};
+
+node::node(/* args */)
+{
+}
+
+node::~node()
+{
+    
+}
